20a91f1e317239c0f5d8dd79f5a737893140e23f
SlavaParamount <viparamount@gmail.com>
2017-05-03 23:54:41 +0300

    поправил код

--- js/form.js
+++ js/form.js
@@ -7,7 +7,7 @@
   var hideElement = window.galleryUtils.hideElement;
   var isElementHidden = window.galleryUtils.isElementHidden;
   var picElement = document.querySelector('.filter-image-preview');
-  var defaultScale = 100;
+  var DEFAULT_SCALE = 100;
   var form = document.querySelector('.upload-filter');
   var uploadForm = document.querySelector('.upload-overlay');
   var commentField = form.querySelector('.upload-form-description');
@@ -25,7 +25,7 @@
     commentField.style.border = '';
     form.reset();
     hideElement(filterLevel);
-    setScale(defaultScale);
+    setScale(DEFAULT_SCALE);
   });
 
   cancelButton.addEventListener('click', function () {
@@ -35,15 +35,11 @@
 
   document.onkeydown = function (evt) {
     if (evt.keyCode === window.ESC_CODE && !isElementHidden(uploadForm) && commentField !== document.activeElement) {
-      applyFilter('none', oldFilter);
-      hideElement(uploadForm);
-      chooseFileForm.reset();
+      closeForm();
     }
 
     if (evt.code === window.ENTER_CODE && cancelButton === document.activeElement) {
-      applyFilter('none', oldFilter);
-      hideElement(uploadForm);
-      chooseFileForm.reset();
+      closeForm();
     }
   };
 
@@ -70,25 +66,25 @@
   hideElement(filterLevel);
 
   var oldFilter;
-  var firstFilterLevel = 0.2;
+  var FIRST_FILTER_LEVEL = 0.2;
 
   document.querySelector('.upload-filter-controls').addEventListener('click', function (evt) {
     if (evt.target.name === 'upload-filter') {
       var filterSelected = evt.target;
       oldFilter = window.initializeFilters(filterSelected, applyFilter, oldFilter);
-      var defaultFilterLevel = document.querySelector('.upload-filter-level-line').offsetWidth * firstFilterLevel;
+      var defaultFilterLevel = document.querySelector('.upload-filter-level-line').offsetWidth * FIRST_FILTER_LEVEL;
       window.setFilterLevel(defaultFilterLevel);
     }
   });
 
   var handler = document.querySelector('.upload-filter-level-pin');
   var progressLine = document.querySelector('.upload-filter-level-val');
-  var lineMaxWidth = 455;
+  var LINE_MAX_WIDTH = 455;
 
   window.setFilterLevel = function (lineWidth) {
     handler.style.left = lineWidth + 'px';
     progressLine.style.width = lineWidth + 'px';
-    var handlerLevel = lineWidth / lineMaxWidth;
+    var handlerLevel = lineWidth / LINE_MAX_WIDTH;
     var stringLevel;
     function applyLevel(string) {
       document.querySelector('.filter-image-preview').style.filter = string;
@@ -125,7 +121,7 @@
       moveEvt.preventDefault();
       var shiftX = startX - moveEvt.clientX;
       window.currentLeft = handler.offsetLeft - shiftX;
-      if ((window.currentLeft > 0) && ((window.currentLeft) < lineMaxWidth)) {
+      if ((window.currentLeft > 0) && ((window.currentLeft) < LINE_MAX_WIDTH)) {
         window.setFilterLevel(window.currentLeft);
       }
       startX = moveEvt.clientX;
@@ -138,14 +134,24 @@
     }
   });
 
-  var minTextLength = 30;
-  var maxTextLength = 150;
+  function closeForm() {
+    applyFilter('none', oldFilter);
+    hideElement(uploadForm);
+    chooseFileForm.reset();
+    window.currentLeft = 0;
+    form.reset();
+  }
+
+  var MIN_TEXT_LENGTH = 30;
+  var MAX_TEXT_LENGTH = 150;
 
   form.addEventListener('submit', function (evt) {
     evt.preventDefault();
     commentField.style.border = '';
-    if ((commentField.value.length < minTextLength) || (commentField.value.length > maxTextLength)) {
+    if ((commentField.value.length < MIN_TEXT_LENGTH) || (commentField.value.length > MAX_TEXT_LENGTH)) { // на chrome выскакивает предупреждение (благодаря атрибуту minlength), на остальных - красная рамка появляется при попытке отправки. если отслеживать input в textarea, то рамка сразу появляется, не очень комфортно для пользователя
       commentField.style.border = '2px solid #CD5C5C';
+    } else {
+      closeForm();
     }
   });
 })();
--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -2,18 +2,18 @@
 (function (window) {
   window.initializeScale = function (scaleElement, setScale) {
     var currentScale = parseInt(document.querySelector('.upload-resize-controls-value').value, 10);
-    var scaleStep = 25;
-    var minScale = 25;
-    var maxScale = 100;
+    var SCALE_STEP = 25;
+    var MIN_SCALE = 25;
+    var MAX_SCALE = 100;
     switch (scaleElement) {
       case document.querySelector('.upload-resize-controls-button-dec'):
-        currentScale -= scaleStep;
-        currentScale = Math.max(currentScale, minScale);
+        currentScale -= SCALE_STEP;
+        currentScale = Math.max(currentScale, MIN_SCALE);
         setScale(currentScale);
         break;
       case document.querySelector('.upload-resize-controls-button-inc'):
-        currentScale += scaleStep;
-        currentScale = Math.min(currentScale, maxScale);
+        currentScale += SCALE_STEP;
+        currentScale = Math.min(currentScale, MAX_SCALE);
         setScale(currentScale);
         break;
     }
